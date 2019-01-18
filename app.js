2985a22aa2b2c87adef8a9ea5099f1e9db8348eb
Leon Ho <leon.whs@gmail.com>
2019-01-18 07:12:33 +0000

    change database to mlab

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ var   commentRoutes     = require("./routes/comments"),
 // Seed Database
 //seedDB();
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+mongoose.connect("mongodb://Leon:2019csjob@ds161074.mlab.com:61074/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
